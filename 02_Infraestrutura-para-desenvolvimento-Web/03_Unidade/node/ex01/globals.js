bf43b0f3d70000f5b975c75f2fa46c391d6781fe
Rafael Rodrigues <rafaelrodriguesousa@gmail.com>
2021-12-20 10:56:19 -0300

    Initial commit

---
+++ 02_Infraestrutura-para-desenvolvimento-Web/03_Unidade/node/ex01/globals.js
@@ -0,0 +1,5 @@
+//Diretório local, onde está o aqrquivo que estamos executando
+console.log(__dirname)
+
+//Caminho completo + nome do arquivo
+console.log(__filename)
---
+++ 03_Desenvolvimento-Front-end/ex006/aula01.js
@@ -0,0 +1,22 @@
+// TIPOS DE DADOS
+let meuNome = "Wesley"; //string
+
+let minhaIdade = 28; // inteiro ou float
+
+let matriculado = true; //booleano
+
+let contratado = null; //valor nulo
+
+let obsevacoes = undefined; //variáveis que não foram definidos valores
+
+let meuArray = []; // array
+
+let meuObjeto = {}; // Objeto
+
+let minhaFunc = function teste(){
+  //função
+};
+
+const minhaCidade = "Porteirinha"; 
+
+console.log (minhaCidade);
