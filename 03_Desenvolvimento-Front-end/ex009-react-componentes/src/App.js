f62cb7a606e09ccfe15ca15e9c1088bb55b76fa0
Rafael Rodrigues <rafaelrodriguesousa@gmail.com>
2022-01-05 11:14:10 -0300

    Create componente in react
    learn how use component js in react

---
+++ 03_Desenvolvimento-Front-end/ex009-react-componentes/src/App.js
@@ -0,0 +1,16 @@
+import MeuComponente from "./MeuComponente";
+
+const App = () => {
+  
+
+  return(
+    <div>
+      <MeuComponente name="Wesley" idade="29"/>
+      <MeuComponente name="Bruno"/>
+      <MeuComponente />
+      <MeuComponente />
+    </div>
+  )
+}
+
+export default App;
---
+++ 03_Desenvolvimento-Front-end/ex009-react-componentes/src/MeuComponente.js
@@ -0,0 +1,9 @@
+function MeuComponente(props){
+  return (
+    <div>
+      <p>Hello Componente {props.name} - {props.idade}</p>
+    </div>
+  )
+}
+
+export default MeuComponente;
