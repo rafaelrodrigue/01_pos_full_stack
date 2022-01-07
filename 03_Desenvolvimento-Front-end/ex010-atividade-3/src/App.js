ec8f7eb89e76f91666d2879b441e834262d0c533
Rafael Rodrigues <rafaelrodriguesousa@gmail.com>
2022-01-07 12:54:43 -0300

    Completion of work

--- 03_Desenvolvimento-Front-end/ex010-atividade-3/src/App.js
+++ 03_Desenvolvimento-Front-end/ex010-atividade-3/src/App.js
@@ -1,7 +1,38 @@
-function App() {
-  return (
-    <div>
-      <h1>Hello world</h1>
+import { useState } from 'react';
+import './styles.css'
+
+function App(){
+
+  const [altura, setAltura] = useState(0);
+  const [peso, setPeso] = useState(0);
+  const [resultado, setResultado] = useState(0);
+
+  const calcular = () =>{
+    let calculo = Number(peso) / Number(altura)**2;
+    setResultado(calculo.toFixed(2));
+  }
+
+  return(
+    <div className='container'>
+
+      <div>
+        
+        <h1>Calculadora de IMC</h1>
+        <h2>Digite sua altura</h2>
+        <input className='box' placeholder='Ex.: 1.70' onChange={(txt) => setAltura(txt.target.value)}/>
+        
+        <h2>Digite seu peso</h2>
+        <input className='box' placeholder='Ex.: 69.5' onChange={(txt) => setPeso(txt.target.value)}/>
+        <br/>
+        <button className='button' onClick={calcular}>Calcular</button>
+
+        {resultado === 0 ? null : <h2>Resultado: {resultado}</h2>}
+
+      </div>
+      <div>
+        <h1>Seu indicador corpóreo é:</h1>
+        {resultado <= 18.5 ? <h2>magreza</h2> : resultado <= 24.9 ? <h2>Normal</h2> : <h2>Sobrepeso</h2>}
+      </div>
     </div>
   );
 }
